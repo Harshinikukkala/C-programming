cf4842ffb84193001166bd53248e96631991420b
Harshini <harshinikukkala@gmail.com>
2023-08-28 23:05:00 +0530

    marks.c file is added

---
+++ conditional-statements/marks.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+   int main () {
+   int  e1,e2,e3,avg;
+   printf("Enter marks in exam1: ");
+   scanf("%d", &e1);
+   printf("Enter marks in exam2: ");
+   scanf("%d", &e2);
+   printf("Enter marks in exam3: ");
+   scanf("%d", &e3);
+   avg=(e1+e2+e3)/3;
+  if (avg>85)
+  {
+   printf("HIGH ACHIEVER");
+ }
+  else if (avg>=70 && avg<=85)
+ {
+  pritnf("PASS");
+ else if (avg<70) 
+{ 
+ printf("NEEDS IMPROVEMENT");
+}
+  return 0;
+} 
+
